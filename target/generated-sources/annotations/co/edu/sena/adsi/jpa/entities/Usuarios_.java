e11f36bf3c92afb312f1c8b1bde5cbbffc469f62
Yurani <yurani@rootbean>
2018-04-11 11:51:17 -0500

    Agrego backend del avance realizado hasta elmomento

---
+++ target/generated-sources/annotations/co/edu/sena/adsi/jpa/entities/Usuarios_.java
@@ -0,0 +1,27 @@
+package co.edu.sena.adsi.jpa.entities;
+
+import co.edu.sena.adsi.jpa.entities.MateriasElectivas;
+import co.edu.sena.adsi.jpa.entities.Roles;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-11T11:10:46")
+@StaticMetamodel(Usuarios.class)
+public class Usuarios_ { 
+
+    public static volatile SingularAttribute<Usuarios, String> apellidos;
+    public static volatile SingularAttribute<Usuarios, Boolean> estado;
+    public static volatile ListAttribute<Usuarios, Roles> rolesList;
+    public static volatile SingularAttribute<Usuarios, String> direccion;
+    public static volatile SingularAttribute<Usuarios, String> documento;
+    public static volatile SingularAttribute<Usuarios, String> codigoUniversitario;
+    public static volatile SingularAttribute<Usuarios, Integer> id;
+    public static volatile ListAttribute<Usuarios, MateriasElectivas> materiasElectivasList;
+    public static volatile ListAttribute<Usuarios, MateriasElectivas> materiasElectivasList1;
+    public static volatile SingularAttribute<Usuarios, String> email;
+    public static volatile SingularAttribute<Usuarios, String> nombres;
+    public static volatile SingularAttribute<Usuarios, String> contraseña;
+
+}
